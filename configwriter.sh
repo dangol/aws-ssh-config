1a7b7075c250d518ddeab76486ab70dda0f15858
Dan <dan@madjic.net>
2018-04-19 15:55:02 -0400

    Runs awsssh.py in cron

---
+++ configwriter.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Put me in /etc/cron.hourly; chmod ug+rx
+AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+TODAY=$(date +%F-%H)
+/usr/local/bin/awsssh.py --private --white-list-region ${AZ:0:-1} --tags Name | sudo tee /etc/ssh/${TODAY}_config > /dev/null
+RES=$(cmp -s /etc/ssh/${TODAY}_config /etc/ssh/ssh_config || echo yes)
+if [ "$RES" == yes ]; then sudo ln -s /etc/ssh/${TODAY}_config /etc/ssh/ssh_config; fi
