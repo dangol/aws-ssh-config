342505e901c97d62dfae4fd82f70930cd28ed002
Dan <dan@madjic.net>
2018-05-09 15:55:03 -0400

    autofind vpcid

--- configwriter.sh
+++ configwriter.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # Put me in /etc/cron.hourly; chmod ug+rx
 AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-VPC=Enter a VPCID here e.g. vpc-d633e6b3
+MAC=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
+VPC=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$MAC/vpc-id)
 TODAY=$(date +%F-%H)
 LNK=$(readlink -f /etc/ssh/ssh_config)
 /usr/local/bin/aws-ssh-config.py --private --white-list-region ${AZ:0:-1} --tags Name --vpc_id ${VPC} | tee /etc/ssh/${TODAY}_config > /dev/null
